e35074772713f931238bbddd793ef5a9ba245a73
Felipe Chung <fchung.dev@gmail.com>
2021-01-30 15:08:51 -0600

    add list-item and card-list components

---
+++ src/components/list-item/list-item.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./list-item.styles.scss";
+
+const ListItem = (props) => (
+  <div
+    style={{ backgroundImage: `url(${props.image})` }}
+    className={`list-item ${props.size}`}
+  >
+    <div className="content">
+      <h1 className="title">{props.title}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+
+export default ListItem;
---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./homepage.styles.scss";
+import CardList from "../../components/card-list/card-list";
+
+const HomePage = () => (
+  <div className="homepage">
+    <CardList></CardList>
+  </div>
+);
+
+export default HomePage;
