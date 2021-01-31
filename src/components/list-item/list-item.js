4453a74a2e7508a32e85815853219b4f79114a9a
Felipe Chung <fchung.dev@gmail.com>
2021-01-31 14:48:07 -0600

    add new div tag to make animation on hover

--- src/components/list-item/list-item.js
+++ src/components/list-item/list-item.js
@@ -2,10 +2,11 @@ import React from "react";
 import "./list-item.styles.scss";
 
 const ListItem = (props) => (
-  <div
-    style={{ backgroundImage: `url(${props.image})` }}
-    className={`list-item ${props.size}`}
-  >
+  <div className={`list-item ${props.size}`}>
+    <div
+      style={{ backgroundImage: `url(${props.image})` }}
+      className="background-image"
+    />
     <div className="content">
       <h1 className="title">{props.title.toUpperCase()}</h1>
       <span className="subtitle">Shop Now</span>
