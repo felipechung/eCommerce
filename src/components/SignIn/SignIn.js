1f575a03f09363942034a5a80959e89f4eade9d0
Felipe Chung <fchung.dev@gmail.com>
2021-07-19 13:58:26 -0300

    create form input

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,8 @@
 import { React, useState } from "react";
 import "./SignIn.scss";
 
+import FormInput from "../FormInput/FormInput";
+
 function SignIn() {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setUserPassword] = useState("");
@@ -13,35 +15,36 @@ function SignIn() {
     setUserPassword(event.target.value);
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    setUserEmail("");
+    setUserPassword("");
+  }
+
   return (
     <div className="sign-in">
       <h2>I already have an account</h2>
       <span>Sign in with your email and password</span>
 
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          setUserEmail("");
-          setUserPassword("");
-          console.log([userEmail, userPassword]);
-        }}
-      >
-        <input
+      <form onSubmit={handleSubmit}>
+        <FormInput
           name="email"
           type="email"
           value={userEmail}
+          label="Email"
           required
           onChange={handleEmailChange}
         />
-        <label>Email</label>
-        <input
+
+        <FormInput
           name="password"
           type="password"
           value={userPassword}
+          label="Password"
           onChange={handlePasswordChange}
           required
         />
-        <label>Password</label>
+
         <input type="submit" value="Submit Form" />
       </form>
     </div>
